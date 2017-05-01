88a31efcef8f7e7bc978b53aca2beb65d257a24e
root <root@localhost.localdomain>
2017-05-01 15:08:15 +0800

    delete unused files

--- start/restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CUR_DIR=$(dirname $(readlink -f $0))
-sh $CUR_DIR/stop.sh
-sleep 1
-sh $CUR_DIR/start.sh
--- start/server_dependency.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-LOGGER_ON=true
-CENTER_ON=true
-LOGIN_ON=true
-GAME_ON=true
-
-GAME_EXIT_PORT=5210
-
